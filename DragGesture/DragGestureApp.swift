834f8f4a193e99b1c2d52deb630066bb79983051
Jonni Åkesson <enterthesource@me.com>
2022-10-12 14:22:08 +0200

    Initial Commit

---
+++ DragGesture/DragGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestureApp.swift
+//  DragGesture
+//
+//  Created by Jonni Akesson on 2022-10-12.
+//
+
+import SwiftUI
+
+@main
+struct DragGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
